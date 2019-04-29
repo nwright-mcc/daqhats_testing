b4d5bc0978f7d183359a5ebad2f4107631b769d6
Nick Wright <nick.wright@mccdaq.com>
2019-04-29 13:45:04 -0400

    Updating code

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# daqhats_testing
+# Tools for testing the MCC 172
+
+To use this code:
+1. Install the daqhats library per instructions.
+2. Clone this repo with:
+   
+   ```sh
+   cd ~
+   git clone https://github.com/nwright-mcc/daqhats_testing.git
+   ```
+
+To acquire sample data to CSV:
+1. Build the finite_scan code:
+   ```sh
+   cd ~/daqhats_testing/c/mcc172/finite_scan
+   make
+   ```
+2. Run `./finite_scan -h` to get the list of options for using the program.
+
--- c/mcc172/finite_scan/finite_scan.c
+++ c/mcc172/finite_scan/finite_scan.c
@@ -1,10 +1,57 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 #include <unistd.h>
 #include <daqhats/daqhats.h>
 
-const char* filename = "test.csv";
+const char* default_filename = "./test.csv";
+
+
+int read_args(int argc, char **argv, uint8_t* address, int* channel, 
+    double* sample_rate_per_channel, uint32_t* samples, char* filename) 
+{
+	int c;
+
+	opterr = 0;
+
+	while ((c = getopt(argc, argv, "ha:c:f:n:o:")) != -1) {
+		switch (c) {
+			case 'a':
+				*address = atoi(optarg);
+				break;
+			case 'c':
+				*channel = atoi(optarg);
+				break;
+            case 'f':
+                *sample_rate_per_channel = atof(optarg);
+                break;
+            case 'n':
+                *samples = strtoul(optarg, NULL, 10);
+                break;
+			case 'h':
+				return 1;
+				break;
+            case 'o':
+                strcpy(filename, optarg);
+                break;
+			case '?':
+				if ((optopt == 'c') || (optopt == 'a') || (optopt == 'f') ||
+                    (optopt == 'n') || (optopt == 'o'))
+					fprintf (stderr, "Option -%c requires an argument.\n", optopt);
+				else if (isprint (optopt))
+					fprintf (stderr, "Unknown option `-%c'.\n", optopt);
+				else
+					fprintf (stderr,  "Unknown option character `\\x%x'.\n", optopt);
+				return 1;
+			default:
+				abort ();
+		}
+	}
+
+	return 0;
+}
 
 int main(int argc, char* argv[])
 {
@@ -16,19 +63,37 @@ int main(int argc, char* argv[])
     uint8_t options;
     uint16_t status;
     double* data;
-    double sample_rate_per_channel = 51200;
+    double sample_rate_per_channel;
     FILE* logfile;
     int index;
     int channel;
     int result;
-    double value;
     uint8_t clock_source;
+    char filename[256];
+    uint8_t synced;
 
-    // Use MCC 172 at address 3
-    address = 3;
-    num_channels = 2;
-    channels = 0x03;
+    sample_rate_per_channel = 51200.0;
     samples_per_channel = 51200;
+    address = 0;
+    channel = 0;
+    strcpy(filename, default_filename);
+    
+	if (read_args(argc, argv, &address, &channel, &sample_rate_per_channel,
+        &samples_per_channel, filename) != 0) 
+    {
+		fprintf(stderr, 
+			"Usage: %s [-a address] [-c channel] [-f frequency] [-n samples] [-o file]\n"
+			"    -a address: address [0-7] of the HAT to scan (default is 0)\n"
+			"    -c channel: channel number to scan (default is 0)\n"
+            "    -f frequency: ADC sampling frequency (default is 51200)\n"
+            "    -n samples: number of samples (default is 51200)\n"
+            "    -o file: output file name (default is ./test.csv)\n",
+            argv[0]);
+		return 1;
+	}
+
+    channels = 1 << channel;
+    num_channels = 1;
     options = 0;
 
     result = mcc172_open(address);
@@ -70,11 +135,21 @@ int main(int argc, char* argv[])
         return 1;
     }
     
-    //mcc172_close(address);
-    //return 0;
+    do
+    {
+        usleep(1000);
+        result = mcc172_a_in_clock_config_read(address, &clock_source, &sample_rate_per_channel, &synced);
+        if (result != RESULT_SUCCESS)
+        {
+            printf("clock_config_read returned %d\n", result);
+            mcc172_close(address);
+            return 1;
+        }
+    } while (synced == 0);
+    printf("ADC clock set to %.1f Hz\n", sample_rate_per_channel);
 #endif
     
-    printf("Scanning %d samples...\n", samples_per_channel);
+    printf("Scanning %d samples from channel %d...\n", samples_per_channel, channel);
     result = mcc172_a_in_scan_start(address, channels, samples_per_channel, options);
     if (result != RESULT_SUCCESS)
     {
@@ -98,7 +173,7 @@ int main(int argc, char* argv[])
         ptr += samples_read_per_channel * num_channels;
         if (samples_read_per_channel > 0)
         {
-            printf("Read: %d %X %d %d\n", result, status, samples_read_per_channel, samples_read);
+            //printf("Read: %d %X %d %d\n", result, status, samples_read_per_channel, samples_read);
         }
         usleep(5000); 
     } while ((result == RESULT_SUCCESS) && ((status & STATUS_RUNNING) == STATUS_RUNNING));
